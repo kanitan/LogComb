ef1d72b26d37abce201df18058f390fb78e7eeb0
penny.lang <penny.lang@zoom.us>
2023-01-31 17:01:09 +0800

    Add example argument.

--- logcomb.py
+++ logcomb.py
@@ -11,6 +11,7 @@ import parse as p
 import argparse
 import configparser
 import os
+import commons
 
 
 def get_args():
@@ -18,7 +19,7 @@ def get_args():
     arg_parser.add_argument(
         '--prefix', '-pre', help='Decide the prefix of parsed log file name')
     arg_parser.add_argument(
-        '--file', '-f', help='Define origin log file to parse.',required=True)
+        '--file', '-f', help='Define origin log file to parse.')
     arg_parser.add_argument(
         '--dir', '-d', help='Define origin log document to parse.')
     arg_parser.add_argument(
@@ -27,18 +28,40 @@ def get_args():
         '--send', '-s', help='Send to ES.', action="store_true")
     arg_parser.add_argument(
         "--logtype",help='See rule_list.py to choose specific log type.')
+    arg_parser.add_argument("--test",'-t',help="Run testing.", action="store_true")
     args = arg_parser.parse_args()
     return args
 
 
 def main():
-    conf_file=os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), 'config.conf')
+    script_path=os.path.dirname(
+            os.path.realpath(__file__))
+    conf_file=os.path.join(script_path, 'config.conf')
     config = configparser.ConfigParser()
     config.read(conf_file)
     args = get_args()
     # args.prefix
     # args.format
+    if args.test:
+        test_path=os.path.join(script_path,'testing/example.log')
+        with open(test_path) as infile:
+            example = infile.readline()
+            test_p = p.log_parser('splunk')
+            result = test_p.entry_parser(example)
+            print(example)
+            # print log
+            commons.print_result(result)
+            print('\n\n====response_body==========================')
+
+            print(result['response_body'])
+            print('====response_body End==========================\n')
+            print('====full_request==========================')
+            print(result['full_request'])
+            print('====full_request End==========================')
+        return
+    if not args.file:
+        print('Please define file path with argument --file/f. Use --help to see details.')
+        return 
     log_type=''
     if config['parser']['log_type']:
         log_type=config['parser']['log_type']
